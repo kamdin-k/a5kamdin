be40740cc5803957164e5d322be3c89d8a28668e
kamdin <kamdinkwkw@gmail.com>
2025-07-23 20:40:26 -0400

    Fix pg error and code issues

--- models/Location.js
+++ models/Location.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 module.exports = (sequelize) => {
   return sequelize.define("Location", {
     name: Sequelize.TEXT,
-    category: Sequelize.TEZT,
+    category: Sequelize.TEXT,
     address: Sequelize.TEXT,
     comments: Sequelize.TEXT,
     image: Sequelize.TEXT
